4a66cf4475a9e7dc042c1c2503a9525498ef82d9
frndotmsn <frndotmsn@jofrix.com>
2022-07-03 23:29:25 +0200

    replaced transition-all using sass instead

--- frontend/src/Datebar.tsx
+++ frontend/src/Datebar.tsx
@@ -14,11 +14,11 @@ const Datebar: Component<DatebarProps> = (props) => {
     return (
         <>
             <div class="fixed grid justify-items-center bottom bottom-0 w-screen h-min z-20">
-                <button type="button" onclick={toggleOnVisible} id="toggle-button" class={`transition-all py-3${visible() ? ' toggled' : ''}`}>
+                <button type="button" onclick={toggleOnVisible} id="toggle-button" class={`py-3${visible() ? ' toggled' : ''}`}>
                     <img src="/src/svg/arrow.svg" alt="Toggle"/>    
                 </button>
             </div>
-            <ul class={`fixed bg-white transition-all ${visible() ? 'top-0' : 'top-full'} min-h-screen w-screen z-10 p-2`}>
+            <ul class={`fixed bg-white ${visible() ? 'top-0' : 'top-full'} min-h-screen w-screen z-10 p-2`}>
                 <For each={props.dates()}>{(date: Date) => 
                     <li><DateComponent date={date} setCurrentDate={props.setCurrentDate} toggleOnVisible={toggleOnVisible} /></li>
                 }</For>
