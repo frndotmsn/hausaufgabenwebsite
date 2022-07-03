df4693bef172a2f531bab824c1dac13785eef6c9
frndotmsn <frndotmsn@jofrix.com>
2022-07-03 23:49:22 +0200

    + spacing between title, creator & updater of task

--- frontend/src/HomeworkTask.tsx
+++ frontend/src/HomeworkTask.tsx
@@ -8,13 +8,15 @@ interface HomeworkTaskProps {
 
 const HomeworkTask: Component<HomeworkTaskProps> = (props) => {
     return (
-        <>
+        <div class="flex justify-between">
             <span>{props.task.title}</span>
-            <UserComponent name={props.task?.creator?.name} id={props.task.creatorId}/>
-            <Show when={props.task.updaterId || props.task.updater}>
-                <UserComponent name={props.task.updater?.name} id={props.task.updaterId} />
-            </Show>
-        </>
+            <div class="grid grid-flow-col justify-center">
+                <UserComponent name={props.task?.creator?.name} id={props.task.creatorId}/>
+                <Show when={props.task.updaterId || props.task.updater}>
+                    <UserComponent name={props.task.updater?.name} id={props.task.updaterId} />
+                </Show>
+            </div>
+        </div>
     )
 };
 export default HomeworkTask;
