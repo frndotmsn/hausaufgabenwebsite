469d0e9372c0a884fe1146f46db0393348529c8f
frndotmsn <frndotmsn@jofrix.com>
2022-07-05 11:00:18 +0200

    changed currentDate from Accessor<Date>to Date

--- frontend/src/HeaderComponent.tsx
+++ frontend/src/HeaderComponent.tsx
@@ -1,15 +1,15 @@
-import { Accessor, Component } from "solid-js";
+import { Component } from "solid-js";
 import { localeDateStringOptions } from "./constants";
 
 interface HeaderComponentProps {
-    currentDate: Accessor<Date>;
+    currentDate: Date;
 }
 
 const HeaderComponent: Component<HeaderComponentProps> = (props) => {
     return (
         <header class="grid p-2 md:p-3 lg:p-4">
             <button class="justify-self-center">
-                {props.currentDate().toLocaleDateString(localeDateStringOptions.locale, localeDateStringOptions.options)}
+                {props.currentDate.toLocaleDateString(localeDateStringOptions.locale, localeDateStringOptions.options)}
             </button>
         </header>
     );
