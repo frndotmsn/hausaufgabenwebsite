804d91dd57142586be2c099cb2d361b872c370e2
frndotmsn <frndotmsn@jofrix.com>
2022-08-15 13:01:02 +0200

    removed old testing code

--- frontend/src/helpers/arrays.ts
+++ frontend/src/helpers/arrays.ts
@@ -1,17 +1,8 @@
-//const groupBy = function<T, K extends keyof any>(list: T[], key: (item: T) => K) {
-//    return list.reduce((groups, item) => {
-//        if(!groups.has(key(item))) groups.set(key(item), [item]);
-//        else groups.get(key(item)).push(item);
-//        return groups;
-//    }, new Map<K, T[]>);
-//}
-
 const groupBy = function<T, K extends keyof any>(list: T[], key: (item: T) => K) {
-    const result = new Map<K, T[]>();
-    for (const item of list) {
-        if(!result.has(key(item))) result.set(key(item), [item]);
-        else result.get(key(item)).push(item);
-    }
-    return result;
+    return list.reduce((groups, item) => {
+        if(!groups.has(key(item))) groups.set(key(item), [item]);
+        else groups.get(key(item)).push(item);
+        return groups;
+    }, new Map<K, T[]>);
 }
 export { groupBy }
