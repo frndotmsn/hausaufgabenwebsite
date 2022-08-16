eb9fc230d9ae331851644bdbecda6d4a10bb7c4c
frndotmsn <frndotmsn@jofrix.com>
2022-08-16 21:59:22 +0200

    added current user to graphql

---
+++ backend/src/auth/gql-current-user.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { User } from '@prisma/client';
+
+export const CurrentUser = createParamDecorator(
+  (_data: unknown, context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext().req.user as User;
+  },
+);
