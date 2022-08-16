0877f0bc0535a35ed6882b3b627c7b59cb199639
frndotmsn <frndotmsn@jofrix.com>
2022-08-16 20:22:44 +0200

    added APIResult helper functions

--- backend/src/models/dto/api-result.ts
+++ backend/src/models/dto/api-result.ts
@@ -4,4 +4,28 @@ interface APIResult<T> {
     message?: string;
 }
 
-export { APIResult };
+const AsSuccessfulAPIResult = <T>(data: T) => {
+    return { success: true, data };
+};
+
+const AsFailedAPIResult = <T>(message: string) => {
+    return { success: true, message };
+};
+
+const AsTryAPIResult = <T>(func: (...args: unknown[]) => T, ...args: unknown[]) => {
+    try {
+        return AsSuccessfulAPIResult(func(...args));
+    } catch (error) {
+        return AsFailedAPIResult(error.message);
+    }
+}
+
+const AsAsyncTryAPIResult = async<T>(func: (...args: unknown[]) => Promise<T>, ...args: unknown[]) => {
+    try {
+        return AsSuccessfulAPIResult(await func(...args));
+    } catch (error) {
+        return AsFailedAPIResult(error.message);
+    }
+}
+
+export { APIResult, AsSuccessfulAPIResult, AsFailedAPIResult, AsTryAPIResult, AsAsyncTryAPIResult };
