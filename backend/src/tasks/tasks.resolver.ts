59901bc52da390b603da84b728b0622f60f65843
frndotmsn <frndotmsn@jofrix.com>
2022-08-17 08:34:55 +0200

    tasks service and resolver use IAPIResult

--- backend/src/tasks/tasks.resolver.ts
+++ backend/src/tasks/tasks.resolver.ts
@@ -1,4 +1,5 @@
-import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { APIResult, APIResultArray, IAPIResult } from 'src/models/dto/api-result';
 import { Task } from 'src/models/task';
 import { TaskCreateInput } from './dto/input/task-create.input';
 import { TaskUpdateInput } from './dto/input/task-update.input';
@@ -8,33 +9,33 @@ import { TasksService } from './tasks.service';
 export class TasksResolver {
     constructor(private readonly tasksService: TasksService) {}
 
-    @Query(() => Task, { nullable: true, name: 'task' })
-    async getTask(@Args('id', { type: () => String }) id: string): Promise<Task | null> {
+    @Query(() => APIResult(Task), { name: 'task' })
+    async getTask(@Args('id', { type: () => String }) id: string): Promise<IAPIResult<Task | null>> {
         return await this.tasksService.getTask(id);
     }
 
-    @Query(() => [Task], { nullable: 'items', name: 'tasks' })
-    async getTasks(@Args('date') date: Date, @Args('toOrFrom') toOrFrom: boolean, @Args('verified') verified: boolean): Promise<Task[]> {
+    @Query(() => APIResultArray(Task), { name: 'tasks' })
+    async getTasks(@Args('date') date: Date, @Args('toOrFrom') toOrFrom: boolean, @Args('verified') verified: boolean): Promise<IAPIResult<Task[]>> {
         return await this.tasksService.getTasks(date, toOrFrom, verified);
     }
 
-    @Query(() => [Date], { name: 'dates' })
-    async getDates(): Promise<Date[]> {
+    @Query(() => APIResultArray(Date), { name: 'dates' })
+    async getDates(): Promise<IAPIResult<Date[]>> {
         return await this.tasksService.getDates();
     }
 
-    @Mutation(() => Task)
-    async createTask(@Args('createTaskData') createTaskData: TaskCreateInput): Promise<Task> {
+    @Mutation(() => APIResult(Task))
+    async createTask(@Args('createTaskData') createTaskData: TaskCreateInput): Promise<IAPIResult<Task>> {
         return await this.tasksService.createTask(createTaskData);
     }
 
-    @Mutation(() => Task)
-    async updateTask(@Args('id', { type: () => String }) id: string, @Args('updateTaskData') updateTaskData: TaskUpdateInput): Promise<Task> {
+    @Mutation(() => APIResult(Task))
+    async updateTask(@Args('id', { type: () => String }) id: string, @Args('updateTaskData') updateTaskData: TaskUpdateInput): Promise<IAPIResult<Task>> {
         return await this.tasksService.updateTask(id, updateTaskData);
     }
 
-    @Mutation(() => Task)
-    async deleteTask(@Args('id', { type: () => String }) id: string): Promise<Task> {
+    @Mutation(() => APIResult(Task))
+    async deleteTask(@Args('id', { type: () => String }) id: string): Promise<IAPIResult<Task>> {
         return await this.tasksService.deleteTask(id);
     }
 }
--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { AsAsyncTryAPIResult, IAPIResult } from 'src/models/dto/api-result';
 import { Task } from 'src/models/task';
 import prisma from 'src/prisma';
 import { TaskCreateInput } from './dto/input/task-create.input';
@@ -6,33 +7,35 @@ import { TaskUpdateInput } from './dto/input/task-update.input';
 
 @Injectable()
 export class TasksService {
-    public async getTask(id: string): Promise<Task | null> {
-        return await prisma.task.findFirst({ where: { id } });
+    public async getTask(id: string): Promise<IAPIResult<Task | null>> {
+        return await AsAsyncTryAPIResult(prisma.task.findFirst, { where: { id } });
     }
     
-    public async getTasks(date: Date, toOrFrom: boolean, verified: boolean): Promise<Task[]> {
-        return await prisma.task.findMany({ where: { ...(toOrFrom ? { dueTo: date} : { issuedAt: date }), verified } });
+    public async getTasks(date: Date, toOrFrom: boolean, verified: boolean): Promise<IAPIResult<Task[]>> {
+        return await AsAsyncTryAPIResult(prisma.task.findMany, { where: { ...(toOrFrom ? { dueTo: date} : { issuedAt: date }), verified } });
     }
 
-    public async getDates(): Promise<Date[]> {
-        const set = new Set<Date>();
-        for (const i of (await prisma.task.findMany({ select: { issuedAt: true, dueTo: true } })))
-        {
-            set.add(i.issuedAt);
-            set.add(i.dueTo);
-        }
-        return Array.from(set);
+    public async getDates(): Promise<IAPIResult<Date[]>> {
+        return await AsAsyncTryAPIResult(async() => {
+            const set = new Set<Date>();
+            for (const i of (await prisma.task.findMany({ select: { issuedAt: true, dueTo: true } })))
+            {
+                set.add(i.issuedAt);
+                set.add(i.dueTo);
+            }
+            return Array.from(set);
+        });
     }
 
     // TODO: check if creator isn't banned
     // TODO: verified & creator based on logged in user
-    public async createTask(taskCreateData: TaskCreateInput): Promise<Task> {
+    public async createTask(taskCreateData: TaskCreateInput): Promise<IAPIResult<Task>> {
         const data = { ...taskCreateData, verified: false, creator: { connect: { id: "1" } } }
-        return await prisma.task.create({ data });
+        return await AsAsyncTryAPIResult(prisma.task.create, { data });
     }
 
     // TODO: verifier & updater based on logged in user
-    public async updateTask(id: string, taskUpdateData: TaskUpdateInput): Promise<Task> {
+    public async updateTask(id: string, taskUpdateData: TaskUpdateInput): Promise<IAPIResult<Task>> {
         const data = {
             ...taskUpdateData,
             updatedAt: new Date(Date.now()),
@@ -43,14 +46,14 @@ export class TasksService {
             createdAt: undefined,
             creator: undefined*/
         };
-        return await prisma.task.update({
+        return await AsAsyncTryAPIResult(prisma.task.update, {
             where: { id },
             data
         });
     }
 
     // TODO: check whether current user has permission to perform the deletion
-    public async deleteTask(id: string): Promise<Task> {
-        return await prisma.task.delete({ where: { id } });
+    public async deleteTask(id: string): Promise<IAPIResult<Task>> {
+        return await AsAsyncTryAPIResult(prisma.task.delete, { where: { id } });
     }
 }
